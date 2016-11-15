5e66616e29085f61e9322984b19b70c03b8e64af
Leonardo Rocha <leo97lar@gmail.com>
2016-11-15 20:31:15 -0200

    JOGO.C
    Ideia inicial do JOGO.C, como discutido em sala

---
+++ JOGO.C
@@ -0,0 +1,43 @@
+#include "jogo.h"
+#define CHECK 1
+#define CHECKMATE 2
+
+
+
+JGO_tpCondRet JGO_IniciarJogo (char* filename, LIS_tppLista listaPecasPossiveis, TAB_tpTabuleiro pTabuleiro){
+
+	char resp;
+	LIS_tppLista listaPecasPossiveis;
+	TAB_tpTabuleiro pTabuleiro;
+
+	pTabuleiro = TAB_AlocarTabuleiro(8, 'H');
+	TAB_CriarTabuleiro (pTabuleiro);
+
+	PCA_InicializarPecas(filename, &listaPecasPossiveis);
+	printf("Padrão? S:N");
+	scanf("%d", resp);
+	if (resp == 'S')
+		montarTabPadrao();
+	else
+		montarTabMod();
+
+}
+
+int main (void){
+	int check;
+	char* ArqPecasPossiveis = "PecasPossiveis.txt";
+	LIS_tppLista listaPecasPossiveis;
+	TAB_tpTabuleiro pTabuleiro;
+
+	JGO_IniciarJogo (ArqPecasPossiveis, listaPecasPossiveis, pTabuleiro);
+
+	do {
+		JGO_RealizarMovimento(listaPecasPossiveis, pTabuleiro);
+		JGO_VerificaCheck(pTabuleiro, &check );
+
+	}while (check != CHECKMATE);
+
+	JGO_FinalizarJogo(listaPecasPossiveis, pTabuleiro);
+
+	return 0;
+}
