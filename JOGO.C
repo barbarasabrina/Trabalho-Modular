84c5036709b12587168ec756234d5f3e770e1c0e
Rudá Fraga Valle <rudavalle@gmail.com>
2016-11-17 21:40:31 -0200

    Adc montartrabmod

--- JOGO.C
+++ JOGO.C
@@ -24,3 +24,39 @@ JGO_tpCondRet JGO_IniciarJogo (char* filename, LIS_tppLista listaPecasPossiveis,
 
 }
 
+void JGO_MontarTabMod(TAB_tpTabuleiro ptabuleiro, LIS_tppLista pecas)
+{
+	int i,k;
+	char  j, *nome, *cor;
+	PCA_tpPeca *peca = (PCA_tpPeca*)malloc(PCA_tpPeca);
+
+	LIS_ObterNo(pecas,peca);
+
+	while (pecas!=NULL)
+	{
+		PCA_ObterCor(peca,cor);
+		PCA_ObterNome(peca,nome);
+		while (peca != NULL)
+		{
+			printf("onde por a peça %c de cor %c", cor, nome);
+			scanf("%d %c", &i, &j);
+			k = TAB_InserirPeca(ptabuleiro, i, j, peca);
+			while (k != 0) {
+				if (k == 1)
+				{
+					printf("casa ocupada tente novamente\n");
+					scanf("%d %c", &i, &j);
+					k = TAB_InserirPeca(ptabuleiro, i, j, peca);
+				}
+				else if (k == 4)
+				{
+					printf("coordenada nao existe tente novamente\n");
+					scanf("%d %c", &i &j);
+					k = TAB_InserirPeca(ptabuleiro, i, j, peca);
+				}
+			}
+		}
+
+	}
+}
+
