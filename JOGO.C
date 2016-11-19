b449b05d1c800b25824121466decc2a7334f0d1b
Leonardo Rocha <leo97lar@gmail.com>
2016-11-19 17:44:29 -0200

    Update JOGO.C
    Colocada a função MostrarTabuleiro e modificado o arquivo para o padrão de um módulo .c da matéria

--- JOGO.C
+++ JOGO.C
@@ -1,9 +1,47 @@
-#include "JOGO.H"
+/***************************************************************************
+*  $MCI Módulo de implementação: JGO jogo de xadrez
+*
+*  Arquivo gerado:              JOGO.c
+*  Letras identificadoras:      JGO
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\JOGO.BSW
+*
+*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: avs, bshmc, rfv, llar
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     4       avs   01/fev/2006 criar linguagem script simbólica
+*     3       avs   08/dez/2004 uniformização dos exemplos
+*     2       avs   07/jul/2003 unificação de todos os módulos em um só projeto
+*     1       avs   16/abr/2003 início desenvolvimento
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+#include   <stdlib.h>
 #include "TABULEIRO.H"
 #include "PECA.H"
 
+#define JOGO_OWN
+#include "JOGO.h"
+#undef JOGO_OWN
+
+/***** Protótipos das funções encapuladas no módulo *****/
+
+/*****  Código das funções exportadas pelo módulo  *****/
 
 
+/***************************************************************************
+*
+*  Função: TAB  &Iniciar Jogo
+*  ****/
 
 JGO_tpCondRet JGO_IniciarJogo (char* filename, LIS_tppLista listaPecasPossiveis, TAB_tpTabuleiro pTabuleiro){
 
@@ -22,7 +60,41 @@ JGO_tpCondRet JGO_IniciarJogo (char* filename, LIS_tppLista listaPecasPossiveis,
 	else
 		JGO_MontarTabMod();//RUDA 
 
-}
+}/* Fim função: TAB &Iniciar Jogo*/
+
+/***************************************************************************
+*
+*  Função: TAB  &Mostrar Tabuleiro
+*  ****/
+
+JGO_tpCondRet JGO_MostrarTabuleiro (TAB_tpTabuleiro ptabuleiro)
+{
+	PCA_tpPeca peca;
+	char nome, cor;
+	int i;
+	char j;
+
+
+	for (i=1; i<=8; i++){
+		printf("|");
+		for (j='A'; j<='H'; j++){
+			TAB_ObterPeca(ptabuleiro, i, j, &peca);
+			if (peca==NULL)
+				printf(" --- |");
+			else {
+				PCA_ObterNome(peca, &nome);
+				PCA_ObterCor(peca, &cor);
+				printf(" %c-%c |", nome, cor);
+			}
+		}
+		printf("\n");
+	}
+}/* Fim função: TAB &Mostrar Tabuleiro*/
+
+/***************************************************************************
+*
+*  Função: TAB  &Montar Tabuleiro Modificado
+*  ****/
 
 void JGO_MontarTabMod(TAB_tpTabuleiro ptabuleiro, LIS_tppLista pecas)
 {
@@ -58,5 +130,8 @@ void JGO_MontarTabMod(TAB_tpTabuleiro ptabuleiro, LIS_tppLista pecas)
 		}
 
 	}
-}
+}/* Fim função: TAB &Montar Tabuleiro Modificado*/
 
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/********** Fim do módulo de implementação: JGO Jogo - Jogo de Xadrez  **********/
